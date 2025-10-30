ac8a2df062f6608d91a9c98044c1d6fa8458b7f2
samuel <samuelmedeiros2025@gmail.com>
2025-10-30 23:46:03 +0000

    feat: criando testes mutantes

---
+++ mutpy_tutorial/test_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from calculator import add, subtract, multiply, divide
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0) # Remova ou comente esta linha
+        self.assertEqual(add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(10, 10), 0)
+        self.assertEqual(subtract(0, 5), -5)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 5), -5)
+        self.assertEqual(multiply(0, 10), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(1, 2), 0.5)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+
